2e459ed9e6a7c2323e2bc2da79d28fd8e5bd5a4b
langthanghy99@gmail.com <langthanghy99@gmail.com>
2022-03-10 20:53:07 +0700

    update

--- action.js
+++ action.js
@@ -81,21 +81,21 @@ let coverN, closeN;
 for(const i in moreNodes) {
 	moreNodes[i].onclick = function(e) {
 		let benefit = benefits[i];
-		benNode.insertAdjacentHTML('beforeend', 
-		   `<div class="cover">
-				<div class="card">
-					<img src="./assets/img/benefit/${benefit[0].avatar}" alt="">
-					<i class="fas fa-times"></i>
-					<h4>${benefit[0].title}</h4>
-					<p>${benefit[0].content}</p>
-					<div class="card--btn">
-					</div>
-				</div>
-			</div>`);
-		let cardbtns = document.querySelector('.card--btn');
-		for(const i in benefit){
-		   cardbtns.innerHTML = cardbtns.innerHTML + '<div class="card--btn__cirle"></div>';
-		}
+		let str = `<div class="cover">
+						<div class="card">
+							<img src="./assets/img/benefit/${benefit[0].avatar}" alt="">
+							<i class="fas fa-times"></i>
+							<h4>${benefit[0].title}</h4>
+							<p>${benefit[0].content}</p>
+							<div class="card--btn">
+							   <div class="card--btn__cirles">
+							      ${'<div class="card--btn__cirle"></div>'.repeat(benefit.length)}
+							   </div>
+							   ${(benefit.length > 1) ? '<i class="fas fa-chevron-right"></i>' : ''}
+							</div>
+						</div>
+					</div>`;
+		benNode.insertAdjacentHTML('beforeend', str);
 		coverN = benNode.querySelector('.cover');
 		closeN = coverN.querySelector('i');
 		coverN.onclick = function(e) {
@@ -106,7 +106,8 @@ for(const i in moreNodes) {
 		                   break;
 		   }
 		}
-		let [ ,...cardbtnList] = cardbtns.childNodes;
+		let cirlesN = document.querySelectorAll('.card--btn__cirle');
+		let [...cardbtnList] = cirlesN;
 		let curCardI = 0;
 		cardbtnList[0].style.backgroundColor = '#00754A';
 		cardbtnList.forEach( (cardbtn) => {
@@ -122,6 +123,7 @@ for(const i in moreNodes) {
 		      cardbtnList[j].style.backgroundColor = '#00754A';
 		      curCardI = j;
 			};
+			
 		});
 	}
 }
